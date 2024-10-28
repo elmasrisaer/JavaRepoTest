f0663b76792fab63ee2897ed67c004630c4bff4c
elmasrisaer <139119606+elmasrisaer@users.noreply.github.com>
2024-10-28 16:48:14 +0200

    liblab SDK update for version v1.1.88 (#2)

--- src/main/java/net/celitech/celitech/Celitech.java
+++ src/main/java/net/celitech/celitech/Celitech.java
@@ -2,8 +2,10 @@ package net.celitech.celitech;
 
 import java.util.concurrent.TimeUnit;
 import net.celitech.celitech.config.CelitechConfig;
+import net.celitech.celitech.hook.CustomHook;
 import net.celitech.celitech.http.Environment;
 import net.celitech.celitech.http.interceptors.DefaultHeadersInterceptor;
+import net.celitech.celitech.http.interceptors.HookInterceptor;
 import net.celitech.celitech.http.interceptors.RetryInterceptor;
 import net.celitech.celitech.services.DestinationsService;
 import net.celitech.celitech.services.ESimService;
@@ -19,6 +21,8 @@ public class Celitech {
   public final PurchasesService purchasesService;
   public final ESimService eSimService;
 
+  private final HookInterceptor hookInterceptor;
+
   public Celitech() {
     // Default configs
     this(CelitechConfig.builder().build());
@@ -27,8 +31,11 @@ public class Celitech {
   public Celitech(CelitechConfig config) {
     final String serverUrl = config.getEnvironment().getUrl();
 
+    this.hookInterceptor = new HookInterceptor(new CustomHook(), config);
+
     final OkHttpClient httpClient = new OkHttpClient.Builder()
       .addInterceptor(new DefaultHeadersInterceptor(config))
+      .addInterceptor(hookInterceptor)
       .addInterceptor(new RetryInterceptor(config.getRetryConfig()))
       .readTimeout(config.getTimeout(), TimeUnit.MILLISECONDS)
       .build();
@@ -48,6 +55,14 @@ public class Celitech {
     this.packagesService.setBaseUrl(baseUrl);
     this.purchasesService.setBaseUrl(baseUrl);
     this.eSimService.setBaseUrl(baseUrl);
+  }
+
+  public void setClientId(String clientId) {
+    this.hookInterceptor.setClientId(clientId);
+  }
+
+  public void setClientSecret(String clientSecret) {
+    this.hookInterceptor.setClientSecret(clientSecret);
   }
 }
 // c029837e0e474b76bc487506e8799df5e3335891efe4fb02bda7a1441840310c
